89de01b3b5ef2b517972efacf79df557be8767a4
dnuno <davidnuno281@gmail.com>
2018-05-24 13:58:58 -0700

    JavaDocs.

--- app/src/main/java/com/davidnuno/cryptomarket/Currency.java
+++ app/src/main/java/com/davidnuno/cryptomarket/Currency.java
@@ -4,9 +4,21 @@ import com.google.gson.annotations.SerializedName;
 
 public class Currency {
 
+    /**
+     * Asset identifier. Superset of the ISO 4217 currency codes standard.
+     */
     @SerializedName("asset_id")
     public String assetId;
 
-    @SerializedName("x")
-    public String x;
+    /**
+     * First trade price inside period range.
+     */
+    @SerializedName("price_open")
+    public double priceOpen;
+
+    /**
+     * Last trade price inside period range.
+     */
+    @SerializedName("price_close")
+    public double priceClose;
 }
